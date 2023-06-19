45cefd0c7b285a96a6abe585f3333d331f2690db
Daniel <daniel.ballesteros.medrano@gmail.com>
2023-06-19 17:37:46 +0200

    Pruebas cuadro de dialogo y borrar datos formulario una vez introducidos

--- src/main/java/com/example/application/views/formulario/Formulario.java
+++ src/main/java/com/example/application/views/formulario/Formulario.java
@@ -3,8 +3,10 @@ package com.example.application.views.formulario;
 import com.example.application.backend.Orden;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -20,13 +22,23 @@ import java.awt.*;
 public class Formulario extends VerticalLayout {
 
     public Formulario() {
+
+        Dialog dialog = new Dialog();
+        dialog.add(new Label("Datos no válidos. Completar todos los campos."));
         IntegerField numero = new IntegerField("Introduzca el numero");
 
         Button boton = new Button("Enviar");
         boton.addClickListener(buttonClickEvent -> {
-            Orden nuevaOrden = new Orden(numero.getValue());
-            Orden.listaOrdenes.add(nuevaOrden);
-            Notification.show("Añadido: " + numero.getValue());
+            if(numero.getValue()==null){
+                dialog.open();
+            }else{
+                Orden nuevaOrden = new Orden(numero.getValue());
+                Orden.listaOrdenes.add(nuevaOrden);
+                Notification.show("Añadido: " + numero.getValue());
+                numero.clear();
+                numero.setInvalid(false);
+            }
+
         });
 
         add(numero, boton);
